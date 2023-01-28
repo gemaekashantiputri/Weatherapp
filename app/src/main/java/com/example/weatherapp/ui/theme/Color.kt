d77d273d3b502c222c7b46dcf14ecb356a11288c
gemaekashantiputri <gemaeka1@gmail.com>
2023-01-28 13:20:52 +0700

    add image drawable folder

---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val black90 = Color(0xFF282828)
+val blacktext90 = Color(0xCC232222)
+val taste100 = Color(0xCC000000)
