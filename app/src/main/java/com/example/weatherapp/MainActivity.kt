78ca5896abae2472ff00b1998996fdd244e315a5
gemaekashantiputri <gemaeka1@gmail.com>
2023-02-01 18:04:28 +0700

    compose interface

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,16 +5,18 @@ import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import com.example.weatherapp.ui.theme.blacktext90
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,18 +37,75 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WeatherPage(name: String) {
-    Column {
+    Column( modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
         HeaderImage()
+        MainInfo()
+        circle()
+        ilustrate()
     }
 }
 
-
 @Composable
 fun HeaderImage() {
-    Image(painter = painterResource(id = R.drawable.ic_glass), contentDescription = null)
+    Image(painter = painterResource(id = R.drawable.ic_glass),
+        contentDescription = null,
+        modifier = Modifier
+            .widthIn(2000.dp)
+            .offset(y = (-50).dp)
+            .fillMaxWidth()
+            .padding(12.dp)
+    )
 }
 
+@Composable
+fun MainInfo() {
+    Text(text = "Banyumas, Indonesia", color = blacktext90,
+        modifier = Modifier
+            .offset(y = (-310).dp)
+    )
+    Text(text = "Today", color = blacktext90,
+        modifier = Modifier
+            .offset(y = (-300).dp)
+    )
+    Text(text = "Temperature", color = blacktext90,
+        modifier = Modifier
+            .offset(y = (-250).dp)
+            .offset(x = (-100).dp)
+        )
+    Text(text = "Wind Speed", color = blacktext90,
+        modifier = Modifier
+            .offset(y = (-200).dp)
+            .offset(x = (-100).dp)
+    )
 
+}
+
+@Composable
+fun circle() {
+    Image(painter = painterResource(id = R.drawable.ic_circle),
+        contentDescription = null,
+        modifier = Modifier
+            .width(1000.dp)
+            .offset(x = (100).dp)
+            .offset(y = (-200).dp)
+    )
+}
+
+@Composable
+fun ilustrate() {
+    Image(painter = painterResource(id = R.drawable.ic_rain),
+        contentDescription = null,
+        modifier = Modifier
+            .offset(y = (-150).dp)
+        )
+    Image(painter = painterResource(id = R.drawable.ic_iconrain), contentDescription = null,
+        modifier = Modifier
+            .offset(y = (-500).dp)
+            .offset(x = (100).dp),
+    )
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
